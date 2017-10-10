2520e9924c948e3b4182f2430d5f50bd53a31a2f
wuwtw <wuwtw@LAPTOP-HFPBD38E.dhcp.wustl.edu>
2017-10-10 18:45:01 -0500

    updated

---
+++ debounce/debounce.ino
@@ -0,0 +1,54 @@
+/*
+  Debounce
+
+  Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
+  press), the output pin is toggled from LOW to HIGH or HIGH to LOW. There's a
+  minimum delay between toggles to debounce the circuit (i.e. to ignore noise).
+
+  The circuit:
+  - LED attached from pin 13 to ground
+  - pushbutton attached from pin 2 to +5V
+  - 10 kilohm resistor attached from pin 2 to ground
+
+  - Note: On most Arduino boards, there is already an LED on the board connected
+    to pin 13, so you don't need any extra components for this example.
+
+  created 21 Nov 2006
+  by David A. Mellis
+  modified 30 Aug 2011
+  by Limor Fried
+  modified 28 Dec 2012
+  by Mike Walters
+  modified 30 Aug 2016
+  by Arturo Guadalupi
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Debounce
+*/
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;    // the number of the pushbutton pin
+const int ledPin = 13;      // the number of the LED pin
+int count=0;
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+void buttonPressed(){
+  if (millis()-lastDebounceTime >= 300){
+    if (digitalRead(buttonPin) != HIGH)
+      Serial.write('y');
+    lastDebounceTime = millis();
+  }
+}
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, CHANGE);
+}
+
+void loop() {
+  Serial.write('x');
+  delay(1000);
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -1,5 +1,9 @@
 package studio6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import jssc.*;
 
 public class SerialComm {
@@ -27,10 +31,59 @@ public class SerialComm {
 	}
 		
 	// TODO: Add writeByte() method from Studio 5
-	
+	public void writeByte(byte x) {
+		try {
+			port.writeByte(x);
+			if (debug) {
+				int i = (byte) x;
+				String y = Integer.toHexString(i);
+				System.out.println("<"+y+">");
+			}
+		}catch (Exception e) {
+				
+		}
+	}
 	// TODO: Add available() method
-	
-	// TODO: Add readByte() method	
+	public boolean available() {
+		try {
+			if(port.getInputBufferBytesCount()>0) {
+				return true;
+			}
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	// TODO: Add readByte() method
+	public byte readByte() {
+		byte[] b= {};
+		try {
+			b = port.readBytes(1);
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return b[0];
+	}
 	
 	// TODO: Add a main() method
+	public static void main(String args[]) {
+		BufferedReader sys = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			boolean indi = true;
+			SerialComm SC = new SerialComm("COM4");
+			while(indi) {
+				if (SC.available()) {
+				while(SC.available()) {
+						char c = (char) SC.readByte();
+						System.out.println(c);
+					}
+				}
+			}
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
